f0ec5c723d3d75e53c0c142f74e74485abc4f0ee
deepakprakash <dp@deepakprakash.com>
2014-08-06 19:36:21 +0200

    Try a different set of bindings for history

--- shell/zsh/keybindings.zsh
+++ shell/zsh/keybindings.zsh
@@ -11,21 +11,21 @@ bindkey -M viins '^e' end-of-line
 # clear the current line then bring it back after the next command
 bindkey -M vicmd 'q' push-line
 
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-  bindkey "${terminfo[kcuu1]}" up-line-or-search      # start typing + [Up-Arrow] - fuzzy find history forward
-fi
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-  bindkey "${terminfo[kcud1]}" down-line-or-search    # start typing + [Down-Arrow] - fuzzy find history backward
-fi
+# if [[ "${terminfo[kcuu1]}" != "" ]]; then
+#   bindkey "${terminfo[kcuu1]}" up-line-or-search      # start typing + [Up-Arrow] - fuzzy find history forward
+# fi
+# if [[ "${terminfo[kcud1]}" != "" ]]; then
+#   bindkey "${terminfo[kcud1]}" down-line-or-search    # start typing + [Down-Arrow] - fuzzy find history backward
+# fi
 
 # bindkey '^R' history-incremental-search-backward
 
-# # search history using up/down arrows
-# autoload -U history-search-end
-# zle -N history-beginning-search-backward-end history-search-end
-# zle -N history-beginning-search-forward-end history-search-end
-# bindkey "\e[A" history-beginning-search-backward-end
-# bindkey "\e[B" history-beginning-search-forward-end
+# search history using up/down arrows
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "\e[A" history-beginning-search-backward-end
+bindkey "\e[B" history-beginning-search-forward-end
 
 # bindkey '^p' history-search-backward
 # bindkey '^n' history-search-forward
