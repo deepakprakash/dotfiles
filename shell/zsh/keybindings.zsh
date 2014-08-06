68530e7f9ff22a7d95b809bfcb7b9d4a003b28c9
deepakprakash <dp@deepakprakash.com>
2014-08-06 16:17:27 +0200

    Enable history keybindings

--- shell/zsh/keybindings.zsh
+++ shell/zsh/keybindings.zsh
@@ -1,29 +1,36 @@
-bindkey -v
+# bindkey -v
 
-bindkey '^R' history-incremental-search-backward
+# bindkey '^R' history-incremental-search-backward
 
-# option + right/left
-bindkey '\e\e[C' vi-forward-word
-bindkey '\e\e[D' vi-backward-word
+# # option + right/left
+# bindkey '\e\e[C' vi-forward-word
+# bindkey '\e\e[D' vi-backward-word
 
-# search history using up/down arrows
-autoload -U history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "\e[A" history-beginning-search-backward-end
-bindkey "\e[B" history-beginning-search-forward-end
+# # search history using up/down arrows
+# autoload -U history-search-end
+# zle -N history-beginning-search-backward-end history-search-end
+# zle -N history-beginning-search-forward-end history-search-end
+# bindkey "\e[A" history-beginning-search-backward-end
+# bindkey "\e[B" history-beginning-search-forward-end
 
-bindkey '^p' history-search-backward
-bindkey '^n' history-search-forward
+# bindkey '^p' history-search-backward
+# bindkey '^n' history-search-forward
 
-# make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey '^?' backward-delete-char
-bindkey '^[[3~' delete-char
-bindkey '^[3;5~' delete-char
-bindkey '\e[3~' delete-char
+# # make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
+# bindkey '^?' backward-delete-char
+# bindkey '^[[3~' delete-char
+# bindkey '^[3;5~' delete-char
+# bindkey '\e[3~' delete-char
 
-bindkey -M viins '^a' beginning-of-line
-bindkey -M viins '^e' end-of-line
+# bindkey -M viins '^a' beginning-of-line
+# bindkey -M viins '^e' end-of-line
 
-# clear the current line then bring it back after the next command
-bindkey -M vicmd 'q' push-line
+# # clear the current line then bring it back after the next command
+# bindkey -M vicmd 'q' push-line
+
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  bindkey "${terminfo[kcuu1]}" up-line-or-search      # start typing + [Up-Arrow] - fuzzy find history forward
+fi
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  bindkey "${terminfo[kcud1]}" down-line-or-search    # start typing + [Down-Arrow] - fuzzy find history backward
+fi
