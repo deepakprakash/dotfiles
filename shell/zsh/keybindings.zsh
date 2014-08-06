17c8b50d93e1118236500856f453c85499827cd8
deepakprakash <dp@deepakprakash.com>
2014-08-06 18:00:56 +0200

    Refactor keybindings file

--- shell/zsh/keybindings.zsh
+++ shell/zsh/keybindings.zsh
@@ -1,11 +1,24 @@
 # Use the vim keybindings
 bindkey -v
 
-# bindkey '^R' history-incremental-search-backward
-
 # option + right/left
 bindkey '\e\e[C' vi-forward-word
 bindkey '\e\e[D' vi-backward-word
+
+bindkey -M viins '^a' beginning-of-line
+bindkey -M viins '^e' end-of-line
+
+# clear the current line then bring it back after the next command
+bindkey -M vicmd 'q' push-line
+
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+  bindkey "${terminfo[kcuu1]}" up-line-or-search      # start typing + [Up-Arrow] - fuzzy find history forward
+fi
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+  bindkey "${terminfo[kcud1]}" down-line-or-search    # start typing + [Down-Arrow] - fuzzy find history backward
+fi
+
+# bindkey '^R' history-incremental-search-backward
 
 # # search history using up/down arrows
 # autoload -U history-search-end
@@ -22,16 +35,3 @@ bindkey '\e\e[D' vi-backward-word
 # bindkey '^[[3~' delete-char
 # bindkey '^[3;5~' delete-char
 # bindkey '\e[3~' delete-char
-
-bindkey -M viins '^a' beginning-of-line
-bindkey -M viins '^e' end-of-line
-
-# clear the current line then bring it back after the next command
-bindkey -M vicmd 'q' push-line
-
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-  bindkey "${terminfo[kcuu1]}" up-line-or-search      # start typing + [Up-Arrow] - fuzzy find history forward
-fi
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-  bindkey "${terminfo[kcud1]}" down-line-or-search    # start typing + [Down-Arrow] - fuzzy find history backward
-fi
