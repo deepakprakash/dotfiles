2a9d32ec2e69de1789da42c0c4e305b713ea1e63
deepakprakash <dp@deepakprakash.com>
2014-08-07 12:12:16 +0200

    Make zsh source ~/.zshrc.local on load
    This will help put in temporary or system specific stuff into
    ~/.zshrc.local

---
+++ shell/local.sh
@@ -0,0 +1,5 @@
+# Source the ~/.zshrc.local script
+# Put all the temporary or system specific actions in your ~/.zshrc.local file.
+# Ideally, once a set of commands in the file are stabilized,
+# refactor them into the ~/.Dotfiles repo and save them
+source $HOME/.zshrc.local
