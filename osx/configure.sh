032e33b8fe005d02210b8909067cf2aa336dd183
Deepak Prakash <dp@deepakprakash.com>
2014-10-23 09:44:29 +0200

    Dock, dashboard & hot corners. Resets included.

--- osx/configure.sh
+++ osx/configure.sh
@@ -452,98 +452,126 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
 	Privileges -bool true
 # Reset: defaults delete com.apple.finder FXInfoPanesExpanded
 
-# ###############################################################################
-# # Dock, Dashboard, and hot corners                                            #
-# ###############################################################################
+###############################################################################
+# Dock, Dashboard, and hot corners                                            #
+###############################################################################
 
-# # Enable highlight hover effect for the grid view of a stack (Dock)
-# defaults write com.apple.dock mouse-over-hilite-stack -bool true
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+# Reset: defaults delete com.apple.dock mouse-over-hilite-stack
 
-# # Set the icon size of Dock items to 36 pixels
-# defaults write com.apple.dock tilesize -int 36
+# Set the icon size of Dock items to 40 pixels
+defaults write com.apple.dock tilesize -int 40
+# Reset: defaults delete com.apple.dock tilesize
 
-# # Change minimize/maximize window effect
+# Change minimize/maximize window effect
 # defaults write com.apple.dock mineffect -string "scale"
+# Reset: defaults delete com.apple.dock mineffect
 
-# # Minimize windows into their application’s icon
-# defaults write com.apple.dock minimize-to-application -bool true
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+# Reset: defaults delete com.apple.dock minimize-to-application
 
-# # Enable spring loading for all Dock items
-# defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+# Enable spring loading for all Dock items
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+# Reset: defaults delete com.apple.dock enable-spring-load-actions-on-all-items
 
-# # Show indicator lights for open applications in the Dock
+# Show indicator lights for open applications in the Dock
+# This is already the default behaviour in Yosemite
 # defaults write com.apple.dock show-process-indicators -bool true
+# Reset: defaults delete com.apple.dock show-process-indicators
 
-# # Wipe all (default) app icons from the Dock
-# # This is only really useful when setting up a new Mac, or if you don’t use
-# # the Dock to launch apps.
-# #defaults write com.apple.dock persistent-apps -array ""
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don’t use
+# the Dock to launch apps.
+# defaults write com.apple.dock persistent-apps -array ""
 
-# # Don’t animate opening applications from the Dock
+# Don’t animate opening applications from the Dock
 # defaults write com.apple.dock launchanim -bool false
+# Reset: defaults delete com.apple.dock launchanim
 
-# # Speed up Mission Control animations
+# Speed up Mission Control animations
 # defaults write com.apple.dock expose-animation-duration -float 0.1
+# Reset: defaults delete com.apple.dock expose-animation-duration
 
-# # Don’t group windows by application in Mission Control
-# # (i.e. use the old Exposé behavior instead)
+# Don’t group windows by application in Mission Control
+# (i.e. use the old Exposé behavior instead)
 # defaults write com.apple.dock expose-group-by-app -bool false
+# Reset: defaults delete com.apple.dock expose-group-by-app
 
-# # Disable Dashboard
+# Disable Dashboard
+# Yosemite doesn't have the dashboard - so not relevant anymore
 # defaults write com.apple.dashboard mcx-disabled -bool true
+# Reset: defaults delete com.apple.dashboard mcx-disabled
 
-# # Don’t show Dashboard as a Space
+# Don’t show Dashboard as a Space
+# Yosemite doesn't have the dashboard - so not relevant anymore
 # defaults write com.apple.dock dashboard-in-overlay -bool true
+# Reset: defaults delete com.apple.dock dashboard-in-overlay
 
-# # Don’t automatically rearrange Spaces based on most recent use
+# Don’t automatically rearrange Spaces based on most recent use
+# Already default in Yosemite
 # defaults write com.apple.dock mru-spaces -bool false
+# Reset: defaults delete com.apple.dock mru-spaces
 
-# # Remove the auto-hiding Dock delay
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# Reset: defaults delete com.apple.dock autohide
+
+# Remove the auto-hiding Dock delay
 # defaults write com.apple.dock autohide-delay -float 0
-# # Remove the animation when hiding/showing the Dock
-# defaults write com.apple.dock autohide-time-modifier -float 0
+# Reset: defaults delete com.apple.dock autohide-delay
 
-# # Automatically hide and show the Dock
-# defaults write com.apple.dock autohide -bool true
+# Reduce the animation delay when hiding/showing the Dock; To remove animation, set as 0
+defaults write com.apple.dock autohide-time-modifier -float 0.5
+# Reset: defaults delete com.apple.dock autohide-time-modifier
 
-# # Make Dock icons of hidden applications translucent
-# defaults write com.apple.dock showhidden -bool true
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+# Reset: defaults delete com.apple.dock showhidden
 
-# # Disable the Launchpad gesture (pinch with thumb and three fingers)
-# #defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+# Disable the Launchpad gesture (pinch with thumb and three fingers)
+# defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+# Reset: defaults delete com.apple.dock showLaunchpadGestureEnabled
 
-# # Reset Launchpad, but keep the desktop wallpaper intact
+# Reset Launchpad, but keep the desktop wallpaper intact
 # find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
 
-# # Add iOS Simulator to Launchpad
+# Add iOS Simulator to Launchpad; Not even sure if this exists anymore
 # sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app" "/Applications/iOS Simulator.app"
 
-# # Add a spacer to the left side of the Dock (where the applications are)
-# #defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
-# # Add a spacer to the right side of the Dock (where the Trash is)
-# #defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
+# Add a spacer to the left side of the Dock (where the applications are)
+# defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+# Add a spacer to the right side of the Dock (where the Trash is)
+# defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
 
-# # Hot corners
-# # Possible values:
-# #  0: no-op
-# #  2: Mission Control
-# #  3: Show application windows
-# #  4: Desktop
-# #  5: Start screen saver
-# #  6: Disable screen saver
-# #  7: Dashboard
-# # 10: Put display to sleep
-# # 11: Launchpad
-# # 12: Notification Center
-# # Top left screen corner → Mission Control
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+# Top left screen corner → Mission Control
 # defaults write com.apple.dock wvous-tl-corner -int 2
 # defaults write com.apple.dock wvous-tl-modifier -int 0
-# # Top right screen corner → Desktop
+# Reset: defaults delete com.apple.dock wvous-tl-corner; defaults delete com.apple.dock wvous-tl-modifier
+
+# Top right screen corner → Desktop
 # defaults write com.apple.dock wvous-tr-corner -int 4
 # defaults write com.apple.dock wvous-tr-modifier -int 0
-# # Bottom left screen corner → Start screen saver
-# defaults write com.apple.dock wvous-bl-corner -int 5
-# defaults write com.apple.dock wvous-bl-modifier -int 0
+# Reset: defaults delete com.apple.dock wvous-tr-corner; defaults delete com.apple.dock wvous-tr-modifier
+
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 10
+defaults write com.apple.dock wvous-bl-modifier -int 0
+# Reset: defaults delete com.apple.dock wvous-bl-corner; defaults delete com.apple.dock wvous-bl-modifier
 
 # ###############################################################################
 # # Safari & WebKit                                                             #
