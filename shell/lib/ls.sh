a8e2fb633e1d6bc942ecb12e254876b620ff1c29
Deepak <dp@deepakprakash.com>
2014-10-29 07:24:27 +0100

    Revamp `ls` handling
    Refactor handling of `ls` to be clean and consistent across different
    platforms and options.

--- shell/lib/ls.sh
+++ shell/lib/ls.sh
@@ -1,8 +1,35 @@
-case $OSTYPE in
-  darwin*)
-    alias ls="ls -FhG" ;;
-  linux*)
-    alias ls="ls -Fh --color=auto" ;;
-esac
+# Functions and aliases for a consistent and clean `ls`.
 
-alias la="ls -al"
+list () {
+    # ls with:
+    #  > colored output (`-G` argument for native OSX `ls`; else `--color=auto`)
+    #  > trailing slash for directories in output (`-F` argument)
+    #  > human readable sizes (`-h` argument) - when using options that show size
+    case $OSTYPE in
+      darwin*)
+        # The native OSX `ls` does not support `--color` argument - hence need
+        # to check if the current `ls` is native or the GNU one from homebrew's
+        # coreutils
+        local ls_path="$(which ls)"
+        if [ "$ls_path" = "/bin/ls" ]; then
+            # Native `ls`
+            ls -FhG "$@"
+        else
+            # GNU `ls` from homebrew
+            ls -Fh --color=auto "$@"
+        fi
+        ;;
+
+      linux*)
+        ls -Fh --color=auto "$@"
+    esac
+}
+
+# An alias for `ls` to `list` seems cleaner than directly having a function
+# called `ls`, since we can always access the raw `ls` if required, by doing
+# `unalias ls`. There also seems to be some weird issues if an `ls` function
+# is created and directly used.
+alias ls="list"
+
+# Convenience aliases
+alias l="ls -al" # Show all files; List view;
