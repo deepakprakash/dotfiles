2cba745db6adfaac0f67b90e44e0d1a1ff4765da
Deepak <dp@deepakprakash.com>
2014-10-29 18:45:29 +0100

    Refactor `ls` stuff

--- shell/lib/ls.sh
+++ shell/lib/ls.sh
@@ -5,6 +5,7 @@ list () {
     #  > colored output (`-G` argument for native OSX `ls`; else `--color=auto`)
     #  > trailing slash for directories in output (`-F` argument)
     #  > human readable sizes (`-h` argument) - when using options that show size
+    local common_args="-Fh"
     case $OSTYPE in
       darwin*)
         # The native OSX `ls` does not support `--color` argument - hence need
@@ -13,15 +14,15 @@ list () {
         local ls_path="$(which ls)"
         if [ "$ls_path" = "/bin/ls" ]; then
             # Native `ls`
-            ls -FhG "$@"
+            ls $common_args -G "$@"
         else
             # GNU `ls` from homebrew
-            ls -Fh --color=auto "$@"
+            ls $common_args --color=auto "$@"
         fi
         ;;
 
       linux*)
-        ls -Fh --color=auto "$@"
+        ls $common_args --color=auto "$@"
     esac
 }
 
@@ -32,4 +33,4 @@ list () {
 alias ls="list"
 
 # Convenience aliases
-alias l="ls -al" # Show all files; List view;
+alias l="ls -Al" # Show all except . and .. (-A); Detailed view(-l);
