ff08f9f166e8c64bb1e4d8d1de012f0aa8960f77
Deepak Prakash <dp@deepakprakash.com>
2015-02-23 14:51:53 +0100

    Add tree aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -13,3 +13,7 @@ alias z="_z -r 2>&1"
 
 # copy with a progress bar.
 alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
+
+# Commonly used `tree` levels
+alias t2="tree -L 2"
+alias t3="tree -L 3"
