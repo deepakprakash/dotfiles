e2529e1fcb7d2b086c603fc84fca8907867baedf
Deepak Prakash <dp@deepakprakash.com>
2015-08-04 17:02:02 +0200

    Fix for OSX specific python and ssh issues due to lack of $LANG and $LC_LANG

--- osx/exports.sh
+++ osx/exports.sh
@@ -12,3 +12,8 @@ if which brew > /dev/null; then
         MANPATH="$coreutils_path/gnuman:$MANPATH"
     fi
 fi
+
+# Fix for Python (https://coderwall.com/p/-k_93g/mac-os-x-valueerror-unknown-locale-utf-8-in-python)
+# and similar issues in SSH, etc
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
