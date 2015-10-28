9225d6826aec20b4e538cf26cf0d9adb90c150fe
Deepak Prakash <deepak@DMac.local>
2015-10-28 08:36:48 +0100

    Modernize the brew installation of tools

--- osx/bootstrap/tools.sh
+++ osx/bootstrap/tools.sh
@@ -3,47 +3,63 @@
 tools=(
 
 ## GNU core utilities (those that come with OS X are outdated)
-coreutils
+# The ` --with-default-names` option does work with coreutils anymore
+"coreutils"
 
-## GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-findutils
-
-## Install more recent versions of some OS X tools
-homebrew/dupes/grep
+## Install more recent/GNU versions of some OS X tools
+"findutils --with-default-names"  # GNU `find`, `locate`, `updatedb`, and `xargs`
+"gnu-indent --with-default-names"
+"gnu-sed --with-default-names"
+"gnu-tar --with-default-names"
+"gnu-which --with-default-names"
+"grep --with-default-names"
+# ed --with-default-names
+# binutils
+"diffutils"
+"gnutls"
+"gzip"
+"gawk"
+# watch
+"wdiff --with-gettext"
+"wget"
 
 ## Essentials
-bash
-python
-node
-tree
-hub
-git
-hg
-zsh
-wget
-ack
-trash
-go
-httpie
-curl
-rsync
-ssh-copy-id
+"bash"
+"zsh"
+
+"hub"
+"git"
+"hg"
+
+"python"
+"go"
+"node"
+
+"vim --override-system-vi"
+
+"tree"
+"ack"
+"trash"
+"ssh-copy-id"
+
+"httpie"
+"curl"
+"rsync"
 
 ## Utils
-graphicsmagick
-webkit2png
-zopfli
-ffmpeg
-# sshfs
-# osxfuse
-mackup
-rename
-https://github.com/soimort/translate-shell
+"graphicsmagick"
+"webkit2png"
+"zopfli"
+"ffmpeg"
+"mackup"
+"rename"
+# https://github.com/soimort/translate-shell
 )
 
 echo "Installing tools..."
-brew install ${tools[@]}
-
+for i in "${tools[@]}"; do
+    brew install $i
+done
 
 echo "Cleaning up..."
 brew cleanup
