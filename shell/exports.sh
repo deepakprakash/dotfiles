9d4fed1c9360b7fc376ef2e866cbbfc16f2c1740
Deepak Prakash <deepak@DMac.local>
2015-10-28 22:25:16 +0100

    Cleanup

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,14 +1,5 @@
 export DOTFILES=~/.dotfiles                 # here
 export ARCHFLAGS='-arch x86_64'             # archflags
-# export PATH=$HOME/bin:/usr/local/bin:$PATH  # path settings
 
 path_prepend "/usr/local/bin"
 path_prepend "$HOME/bin"
-
-# path_add "/usr/local/sbin"
-# path_add "/usr/local/share/npm/bin"
-
-# if type go >/dev/null 2>&1; then
-#   export GOPATH="$(realpath `which go` | sed "s/\/libexec\/bin\/go//")"
-#   path_add "$GOPATH/bin"
-# fi
