ffe110508ea7cd8e8f2a75232ff889769f7ace92
Deepak Prakash <deepak@dmac.home>
2017-03-18 17:28:45 +0100

    Rearrangement, documentation for tools installation

--- osx/bootstrap/tools.sh
+++ osx/bootstrap/tools.sh
@@ -3,10 +3,14 @@
 tools=(
 
 ## GNU core utilities (those that come with OS X are outdated)
-# The ` --with-default-names` option does work with coreutils anymore
+# The ` --with-default-names` option does work with coreutils - but we have
+# the mechanism in shell/exports.sh that makes sure that the coreutils is added to PATH
 "coreutils"
 
-## Install more recent/GNU versions of some OS X tools
+## Install more recent/GNU versions of some OS X tools.
+
+# These are installed with the --with-default-names option since they all have
+# separate install locations and adding each one to PATH is cumbersome and error prone
 "findutils --with-default-names"  # GNU `find`, `locate`, `updatedb`, and `xargs`
 "gnu-indent --with-default-names"
 "gnu-sed --with-default-names"
@@ -14,49 +18,52 @@ tools=(
 "gnu-which --with-default-names"
 "grep --with-default-names"
 # ed --with-default-names
-# binutils
+
+# "binutils"
 "diffutils"
 "gnutls"
 "gzip"
 "gawk"
-# watch
+"watch"
 "wdiff --with-gettext"
-"wget"
 
 ## Essentials
 "bash"
 "zsh"
-
-"hub"
 "git"
 "hg"
 
+"curl"
+"wget"
+
+## Languages
 "python"
+"ruby"
 "go"
 "node"
 
+## Editors
 "vim --override-system-vi"
 
+## System Utils
 "tree"
 "ack"
 "trash"
 "ssh-copy-id"
-
-"httpie"
-"curl"
-"rsync"
-
+"htop"
 "iproute2mac"
 
-## Utils
-"terminal-notifier"
-"graphicsmagick"
-"webkit2png"
-"zopfli"
-"ffmpeg"
+## General Utils
+"httpie"
+"rsync"
 "mackup"
 "rename"
-"htop-osx"
+
+# "terminal-notifier"
+# "graphicsmagick"
+# "webkit2png"
+# "zopfli"
+# "ffmpeg"
 # https://github.com/soimort/translate-shell
 )
 
