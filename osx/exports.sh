ea8d08ca3b5fab489861c9dda3ca54da63ceabe3
Deepak Prakash <deepak@dmac.home>
2017-03-18 13:59:15 +0100

    Move the append for the gnucoreutils to the PATH to the common exports.sh
    - Now export a variable called GNU_COREUTILS_PATH when its present
    
    - Additionally fix the export for the MANPATH

--- osx/exports.sh
+++ osx/exports.sh
@@ -1,18 +1,4 @@
 
-# Setup OSX to use the brew coreutils
-if which brew > /dev/null; then
-    # Brew is installed
-    coreutils_path="$(brew --prefix coreutils)/libexec"
-
-    if [ -d $coreutils_path ]; then
-        # Prepend to path
-        path_prepend "$coreutils_path/gnubin"
-
-        # Prepend to MANPATH for correct manpages
-        MANPATH="$coreutils_path/gnuman:$MANPATH"
-    fi
-fi
-
 # Fix for Python (https://coderwall.com/p/-k_93g/mac-os-x-valueerror-unknown-locale-utf-8-in-python)
 # and similar issues in SSH, etc
 export LC_ALL=en_US.UTF-8
--- shell/exports.sh
+++ shell/exports.sh
@@ -3,3 +3,25 @@ export ARCHFLAGS='-arch x86_64'             # archflags
 
 path_prepend "/usr/local/bin"
 path_prepend "$HOME/bin"
+
+
+# local GNU_COREUTILS_PATH = "/usr/local/opt/coreutils/libexec/gnubin"
+# if [ -d "$GNU_COREUTILS_PATH" ]; then
+#   path_prepend "/usr/local/opt/coreutils/libexec/gnubin"
+# fi
+
+
+# Setup OSX to use the brew coreutils
+# if which brew > /dev/null; then
+    # Brew is installed
+    coreutils_prefix="$(brew --prefix coreutils 2>/dev/null)"
+
+    if [ -d "$coreutils_prefix/libexec/gnubin" ]; then
+        # Prepend to path
+        export GNU_COREUTILS_PATH="$coreutils_prefix/libexec/gnubin"
+        path_prepend "$GNU_COREUTILS_PATH"
+
+        # Prepend to MANPATH for correct manpages
+        export MANPATH="$coreutils_prefix/libexec/gnuman:$MANPATH"
+    fi
+# fi
