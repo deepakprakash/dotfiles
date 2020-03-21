ed710c17c447ac65ffde359f95f6a0268789d0a9
Deepak Prakash <dp@deepakprakash.com>
2020-03-21 18:41:52 +0100

    Remove comments

--- shell/fish/functions/remove.fish
+++ shell/fish/functions/remove.fish
@@ -1,8 +1,6 @@
 
 function remove --argument-names variable value --description "Remove all occurences of the second argument from variable corresponding to first argument."
-    # echo (contains --index $value $$variable)
     while set index (contains --index $value $$variable)
-        echo '$index: ' $index
         set --erase $variable\[$index\]
     end
 end
