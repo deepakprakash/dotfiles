7f25734d206a868482dbbff223d3be14952c18ee
Deepak Prakash <dp@deepakprakash.com>
2020-03-22 20:37:43 +0100

    Use the new prompt_status instead and remove the postexec summary

--- shell/fish/functions/fish_prompt.fish
+++ shell/fish/functions/fish_prompt.fish
@@ -124,7 +124,7 @@ function fish_prompt_first_line
 
     printf (set_color --dim grey)'\n#'(set_color normal)
 
-    for section in (prompt_user_host) (prompt_git) (prompt_python_virtualenv) (prompt_jobs) (prompt_date)
+    for section in (prompt_user_host) (prompt_git) (prompt_python_virtualenv) (prompt_jobs) (prompt_date) (prompt_status)
         if test -z $section
             continue
         end
@@ -147,30 +147,5 @@ function fish_prompt
     fish_prompt_first_line
 
     printf (set_color --dim grey)'› '(set_color normal)(prompt_pwd_custom_colored)(set_color cyan)' » '(set_color normal) # › ❯ ▶ ➞ ➡ ⇒ ➡ 
-
-end
-
-
-function previous_command_summary --on-event fish_postexec
-
-    # Save the status for later use.
-    set --local real_status $status
-    set --local real_cmd_duration $CMD_DURATION
-
-    # argv[1] contains the full commandline. We check if it is empty ot just white space
-    # and move forward only if it is not. This is a hack since fish does not update the status
-    # and cmd_duration in case of no-op commands such as just a carriage return or comment
-    # Since this is a hack, it fails for the comment use case for instance.
-    if test -z (string trim $argv[1])
-        return 0
-    end
-
-    if test $real_status -ne 0
-        printf (set_color $fish_color_error)'\nExit Code: %s\n'(set_color normal) $real_status
-    end
-
-    if test $real_cmd_duration -gt 2000
-        printf (set_color --dim $fish_color_comment)'\nTime: %ssec\n'(set_color normal) (math $real_cmd_duration/1000)
-    end
 
 end
